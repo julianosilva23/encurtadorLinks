2b3eb83a4f2c44684b493d2102e05c27b250e625
Juliano Silva <julianoluizsilva00@gmail.com>
2020-06-14 21:10:27 -0300

    Adicionar cors, remover banco e resultado dos testes

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import './bootstrap';
 import Youch from 'youch';
 import 'express-async-errors';
 import express from 'express';
+import cors from 'cors';
 import routes from './routes';
 
 import './database';
@@ -18,6 +19,7 @@ class App {
     }
 
     middlewares() {
+        this.server.use(cors());
         this.server.use(express.json());
     }
 
